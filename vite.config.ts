45100bbca8e4be1bbbf33351250a3153b1f01d31
ray <3086745286@qq.com>
2025-03-04 19:36:17 +0800

    elmUI-按需引入

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import { fileURLToPath, URL } from "node:url";
+//element-plus按需引入
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
-import VueSetupExtend from "vite-plugin-vue-setup-extend"; //给setup语法糖命名
+//给setup语法糖命名
+import VueSetupExtend from "vite-plugin-vue-setup-extend";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), VueSetupExtend()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VueSetupExtend(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
