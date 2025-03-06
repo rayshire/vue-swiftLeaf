b80ffca9983dff1853390d7fa96ad6d6bed2069a
ray <3086745286@qq.com>
2025-03-06 10:28:39 +0800

    用vueuse实现吸顶导航

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <!-- 二级路由出口 -->
+    <LayoutFixed />
     <LayoutNav />
     <LayoutHeader />
     <RouterView />
@@ -13,6 +14,7 @@
 import LayoutNav from './components/LayoutNav.vue';
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue';
 
 
 </script>
