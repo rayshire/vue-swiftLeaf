4cfc2138cef0292275eff0e26755a515ea712b33
ray <3086745286@qq.com>
2025-03-06 20:03:00 +0800

    封装goodsitem商品组件

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -11,13 +11,8 @@
           </strong>
         </RouterLink>
         <ul class="goods-list">
-          <li v-for="good in cate.goods.slice(0, 6)" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy="good.picture" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+          <li v-for="goods in cate.goods.slice(0, 6)" :key="goods.id">
+            <GoodsItem :goods="goods" />
           </li>
         </ul>
       </div>
@@ -27,6 +22,7 @@
 
 <script setup lang="ts" name="HomeProduct">
 import HomePanel from './HomePanel.vue'
+import GoodsItem from './Goodsitem.vue'
 import { getGoodsAPI } from '@/apis/home'
 import { ref } from 'vue'
 const goodsProduct = ref([])
@@ -129,42 +125,6 @@ getGoodsProduct()
         &:nth-child(4n) {
           margin-right: 0;
         }
-      }
-    }
-
-    .goods-item {
-      display: block;
-      width: 220px;
-      padding: 20px 30px;
-      text-align: center;
-      transition: all .5s;
-
-      &:hover {
-        transform: translate3d(0, -3px, 0);
-        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-      }
-
-      img {
-        width: 160px;
-        height: 160px;
-      }
-
-      p {
-        padding-top: 10px;
-      }
-
-      .name {
-        font-size: 16px;
-      }
-
-      .desc {
-        color: #999;
-        height: 29px;
-      }
-
-      .price {
-        color: $priceColor;
-        font-size: 20px;
       }
     }
   }
