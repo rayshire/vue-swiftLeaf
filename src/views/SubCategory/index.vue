7ac98fd3489b17af42287388d967a150b21e100b
ray <3086745286@qq.com>
2025-03-07 19:01:55 +0800

    二级分类-商品列表（无限）加载

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -15,7 +15,7 @@
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
         <!-- 商品列表-->
         <GoodsItem v-for="item in goodList" :key="item.id" :goods="item" />
       </div>
@@ -76,6 +76,26 @@ function tabChange() {
   reqData.value.page = 1
   getGoodList()
 }
+//无限加载
+const disabled = ref(false)
+async function load() {
+  // 如果已经加载了三页数据，停止加载
+  if (reqData.value.page >= 3) {
+    disabled.value = true
+    return
+  }
+  // 获取下一页的数据
+  reqData.value.page++
+  // 深拷贝response，解决该死的ts类型检查
+  const res = JSON.parse(JSON.stringify(await getSubCategoryAPI(reqData.value)))
+  // 将新数据添加到列表中,使用展开运算符
+  goodList.value = [...goodList.value, ...res.result.items]
+  // // 加载完毕 停止监听
+  // if (res.result.items.length === 0) {
+  //   disabled.value = true
+  // }
+}
+
 </script>
 
 <style scoped lang="scss">
